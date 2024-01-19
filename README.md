f5f1d4bb552f59757c6b8d829c505e6b1925b0ac
bimpop <bimpop>
2024-01-20 00:12:51 +0100

    final clean

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # simpleDither
-a simple and exhaustive dithering program
+## what is it ?
+a simple and exhaustive dithering program that feature a lot of different algorithm and color palette!
+
+## how do I use it ?
+```python
+import simpleDither as sd
+
+img_path = 'path/to/image.JPG'
+
+# create a dimg instance
+img = sd.dimg(img_path)
+
+# select a palette (default is black and white)
+img.palette = sd.GAMEBOY
+
+# resize to see those juicy pixels
+img.resize(target_width=160)
+
+# apply the algorithm of your choice
+img.bayer(16)
+
+# save the result !
+img.save()
+```
--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python
-import numpy as np
-from multiprocessing import Pool
-
-import image_processing as dither
+import simpleDither as sd
 
 img_path = 'IN/DSCF9150.JPG'
 
-img = dither.dimg(img_path)
-img.palette = dither.GAMEBOY
-# img.square_crop()
+img = sd.dimg(img_path)
+img.palette = sd.GAMEBOY
 img.resize(target_width=160)
 img.bayer(16)
-# img.fs()
-# img.atk()
-# img.stucki()
-# img.burkes()
-# img.sierra()
 img.save()
-
-
-exit()
-
-todo = [
-    ['IN/DSCF7667.JPG',(380, 380), pal.BW,'simple',   16, 0.5]
-]
-
-if __name__ == '__main__':
-    with Pool() as p:
-        p.map(dither.processPicture_paralel, todo)
-    
